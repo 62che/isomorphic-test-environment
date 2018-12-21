77d77bdacb9c3fc3d79f4af2bca4a50a50bff331
Wonho Jang <jwh1807@gmail.com>
2018-12-21 15:49:00 +0900

    add scripts

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,15 @@
 // Karma configuration
 // Generated on Tue Dec 18 2018 09:45:49 GMT+0900 (대한민국 표준시)
 
-module.exports = function(config) {
-  console.log(process.env.TEST);
-  console.log(process.env.NODE_ENV);
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,11 +17,8 @@ module.exports = function(config) {
       'test/**/*.test.js'
     ],
 
-
     // list of files / patterns to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -36,62 +29,52 @@ module.exports = function(config) {
 
     webpack: {
       module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules|\.test\.js|index.js/,
-            use: [
-              {loader: 'babel-loader'},
-              {loader: 'istanbul-instrumenter-loader', options: {esModules: true}}
-            ]
-          }
-        ]
+        rules: [{
+          test: /\.js$/,
+          exclude: /node_modules|\.test\.js|index.js/,
+          use: [
+            { loader: 'babel-loader' },
+            { loader: 'istanbul-instrumenter-loader', options: { esModules: true } }
+          ]
+        }]
       }
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: process.env.COVERAGE ? ['mocha', 'coverage'] : ['mocha'],
 
-
     coverageReporter: {
       // specify a common output directory
       dir: 'coverage/browser',
       reporters: [
         { type: 'html' },
-        { type: 'text' },
+        { type: 'text' }
         // { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
       ]
     },
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: process.env.COVERAGE ? true : false,
+    singleRun: !!process.env.COVERAGE,
 
     // Concurrency level
     // how many browser should be started simultaneous
