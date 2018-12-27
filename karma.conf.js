b4fd70837fc96a8e11cb87aa19d45804d414591a
62che <the62che@gmail.com>
2018-12-27 13:20:43 +0900

    coverage case

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: process.env.COVERAGE ? ['mocha', 'coverage'] : ['mocha'],
-    reporters: ['mocha', 'karma-typescript'],
+    reporters: process.env.COVERAGE ? ['mocha', 'karma-typescript'] : ['mocha'],
 
     karmaTypescriptConfig: {
       reports:
