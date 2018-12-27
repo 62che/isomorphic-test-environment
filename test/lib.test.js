008dfb84acc0fbe9811b20c49319be608260d06e
62che <the62che@gmail.com>
2018-12-27 11:15:10 +0900

    separate mocha, nyc config

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,4 +1,5 @@
 import { assert, expect } from 'chai'
+import 'chai/register-should'
 import { lib } from '../'
 
 describe('lib', function() {
@@ -9,6 +10,10 @@ describe('lib', function() {
 
     it('bdd style', function() {
       expect(lib.add(1, 2)).to.equal(3)
+    })
+
+    it('bdd should style', function () {
+      lib.add(1, 2).should.equal(3)
     })
   })
 })
